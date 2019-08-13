82997ccf117f7e533925034ca0e5bd935fe16585
zulhafiz06 <zulhafiz06@gmail.com>
2019-08-13 23:46:08 +0800

    start 82

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,29 @@
 "use strict";
-console.log("123");
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                // All of this only works if collection is number []
+                if (this.collection instanceof Array) {
+                    // collection === number[]
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // only going to work if collection is a string
+                if (typeof this.collection === 'string') {
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
