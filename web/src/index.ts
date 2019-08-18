313031a98890e02ea7a771b06fde5852457cf04a
zulhafiz06 <zulhafiz06@gmail.com>
2019-08-19 00:52:50 +0800

    latest

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 20 });
+const user = new User({ name: 'new record', age: 0 });
 
-user.set({ name: 'newName' });
+user.events.on('change', () => {
+  console.log('change!');
+});
 
-console.log(user.get('name'));
+user.events.trigger('change');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,16 +1,15 @@
-interface UserProps {
+import { Sync } from './Sync';
+import { Eventing } from './Eventing';
+
+export interface UserProps {
+  id?: number;
   name?: string;
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 }
